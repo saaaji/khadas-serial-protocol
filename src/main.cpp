8a531a6706dcba2bd45ac07b8676746474bef961
Edge K4 <curobotics@colorado.edu>
2023-11-07 21:52:53 -0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 #undef winsize
 #undef termio
 
+#include <poll.h>
+#include <sys/time.h>
 #include "SerialComms.hpp"
 #include <chrono>
 #include <string>
 
-constexpr int CYCLE_FREQ_HZ = 1000; // Hz
+constexpr int CYCLE_FREQ_HZ = 2000; // Hz
 constexpr int PACKET_DUMP_COUNT = 150;
 
 uint32_t bit_cast(float f) {
@@ -25,93 +27,195 @@ uint32_t bit_cast(float f) {
 typedef std::chrono::high_resolution_clock sys_time;
 typedef std::chrono::duration<uint32_t, std::micro> micros;
 
-int main(int argc, char **argv) {
-    int cycle_time_us = 1'000'000 / CYCLE_FREQ_HZ;
-
-    if (argc < 2)
-        return 1;
-
-    int payload_size = std::atoi(argv[1]);
-    char *payload = new char[payload_size]; // n bytes
-
-    for (int i = 0; i < payload_size; i++) {
-        payload[i] = static_cast<char>(0);
-    }
-    payload[0] = 1;
-
-    int port = open("/dev/ttyACM0", O_RDWR);
-
+// if return value is less than 0, check errno
+// https://www.gnu.org/software/libc/manual/html_node/Setting-Modes.html
+int init_uart(int port) {
     struct termios tty;
-    speed_t brate = B38400;
-    tcgetattr(port, &tty);
-    
-    cfsetospeed(&tty, brate);
-    cfsetispeed(&tty, brate);
 
-    tty.c_cflag |= (CLOCAL | CREAD);
-    tty.c_cflag &= ~PARENB;    // No parity
-    tty.c_cflag &= ~CSTOPB;    // 1 stop bit
+    if (tcgetattr(port, &tty) < 0) {
+        return -1;
+    }
+
+    // "raw" mode
+    tty.c_cflag |= (CLOCAL | CREAD); // ignore modem control lines, enable receiver
     tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;        // 8 data bits
-    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    tty.c_oflag &= ~OPOST;
+    tty.c_cflag |= CS8; // 1 "char" = 8 bits
+    tty.c_cflag &= ~PARENB; // disable bit parity check
+    // tty.c_cflag |= CSTOPB; // set 2 stop bits
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS; // disable HW flow control (RTS)
+    // tty.c_cflag |= CRTSCTS; // enable HW flow control (RTS)
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    tty.c_oflag &= ~OPOST; // disable implementation-based output processing
+
     tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 0; // NOBLOCK
+    tty.c_cc[VTIME] = 0;
+
+    if (tcsetattr(port, TCSANOW, &tty) < 0) {
+        return -1;
+    }
+
+    /*
+    man page: "tcsetattr() returns success if any of the requested changes could 
+    successfully be carried out...when making multiple changes...follow this call with a
+    further call to tcgetattr to check that all changes have been performed successfully"
+    */
+    if (tcgetattr(port, &tty) < 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+// if return value is less than 0, check errno
+// https://man7.org/linux/man-pages/man2/ioctl_tty.2.html
+int config_uart_bitrate(int port, int bitrate) {
+    struct termios2 tty;
+
+    // ioctl equivalent to "tc(get/set)attr"
+    if (ioctl(port, TCGETS2, &tty) < 0) {
+        return -1;
+    }
+
+    /*
+    man page: "If...c_cflag contains the flag BOTHER, then the baud rate is
+    stored in the structure members c_ispeed and c_ospeed as integer values"
+    */
+    tty.c_cflag &= ~CBAUD;
+    tty.c_cflag |= BOTHER;
+    tty.c_ispeed = bitrate;
+    tty.c_ospeed = bitrate;
+
+    if (ioctl(port, TCSETS2, &tty) < 0) {
+        return -1;
+    }
+
+    if (ioctl(port, TCGETS2, &tty) < 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int elapsed_us(struct timeval st) {
+    struct timeval et;
+    gettimeofday(&et, NULL);
+    return (et.tv_sec - st.tv_sec) * 1'000'000 + (et.tv_usec - st.tv_usec);
+}
+
+int main(int argc, char **argv) {
+    int bitrate = 480'000'000; // 480 Mbps
+
+    if (argc < 3) {
+        printf("expected <payload_size> <divisor>\n");
+        return 1;
+    }
+
+    // initialize UART
+    int port = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
+
+    if (port < 0) {
+        printf("open failed: %d\n", errno);
+        return 1;
+    }
+
+    if (init_uart(port) < 0) {
+        printf("init_uart failed\n");
+        return 1;
+    }
+
+    if (config_uart_bitrate(port, bitrate) < 0) {
+        printf("config_uart_bitrate failed\n");
+        return 1;
+    }
+
+    // allocate payload
+    int payload_size = std::atoi(argv[1]);
+    unsigned char *raw_bytes = reinterpret_cast<unsigned char*>(calloc(sizeof(unsigned char), 100000)); // n bytes
+    raw_bytes[0] = 1;
     
-    tcsetattr(port, TCSANOW, &tty);
+    int size = 4095;
+    int recv = 0;
+    unsigned char buffer[size];
 
-    struct termios2 tty2;
-    ioctl(port, TCGETS2, &tty2);
-    tty2.c_cflag &= ~CBAUD;
-    tty2.c_cflag |= BOTHER;
-    tty2.c_ispeed = 480000000;
-    tty2.c_ospeed = 480000000;
-    // tty2.c_cc[VMIN] = 0;
-    // tty2.c_cc[VTIME] = 0;
-    // tty2.c_cflag |= (CLOCAL | CREAD);
-    // tty2.c_cflag &= ~PARENB;    // No parity
-    // tty2.c_cflag &= ~CSTOPB;    // 1 stop bit
-    // tty2.c_cflag &= ~CSIZE;
-    // tty2.c_cflag |= CS8;        // 8 data bits
-    // tty2.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    // tty2.c_oflag &= ~OPOST;
-    ioctl(port, TCSETS2, &tty2);
-    int blen = 4096;
-    char bytes[blen];
+    int sent = 0;
+    int freq = 2000;
+    int cycle_time_us = 1'000'000 / freq;
+    struct timeval gst;
+    gettimeofday(&gst, NULL);
 
-    for (int i = 0; i < 1; i++) {
-        // send n bytes
-        int res = write(port, payload, payload_size);
-        // tcdrain(port);
-        printf("WBUF: %d\n", res);
+    int theoretical_limit = 60'000'000 / freq;
+    int limit = theoretical_limit / std::atoi(argv[2]);
+    int prev_recv = -1;
 
-        int recv = 0;
-        int streak = 0;
+    for (int i = 0;;i++) {
+        struct timeval st, et;
+        gettimeofday(&st, NULL);
 
-        // printf("RBUF:");
-        int j = 0;
-        while (true) {
-            j++;
-            auto start = sys_time::now();
-            int res = read(port, bytes, blen);
-            if (res > 0) {
-                // printf(" %d", res);
-                streak = 0;
-                recv += res;
-            } else if (res == 0) {
-                streak++;
+        if (sent < payload_size) {
+            // teensy can send back data while we are writing, causing potential loss
+            int res = write(port, raw_bytes, limit);
+            raw_bytes[0] = 0;
+            if (res < limit) {
+                printf("write failed: %d\n", errno);
+                return 1;
+            } else {
+                sent += res;
             }
-            
-            if (j%1000==0) printf("R: %d\n", recv);
-            if (recv >= payload_size) break;
-            while (std::chrono::duration_cast<micros>(sys_time::now() - start).count() < cycle_time_us);
+        } else if (raw_bytes != nullptr) {
+            free(raw_bytes);
+            raw_bytes = nullptr;
         }
 
-        printf("\nTRIAL %d > recv: %d / %d\n\n", i, recv, payload_size);
+        int available = 0, tot = 0, n = 0;
+        int res;
+        do {
+            res = read(port, buffer, size);
+            if (res > 0) {
+                tot += res;
+                recv += res;
+                n++;
+            }
+        } while (res > 0);
+
+        /*ioctl(port, FIONREAD, &available);
+        while (available > 0 && elapsed_us(st) < cycle_time_us) {
+            int res = read(port, buffer, size);
+            if (res > 0) {
+                recv += res;
+                tot += res;
+                n++;
+            }
+            ioctl(port, FIONREAD, &available);
+        }*/
+
+        if (i % 5 == 0 && recv != prev_recv) {
+            printf("RECV: %d / %d (%.2f) (%d) : ", recv, sent, float(recv)/sent, n);
+        }
+
+        if (recv == sent) break;
+
+        while (elapsed_us(st) < cycle_time_us) {
+            continue;
+        }
+
+        if (i % 5 == 0 && recv != prev_recv) {
+            float elapsed = float(elapsed_us(gst)) / 1'000'000;
+            printf("%.0f : %.3f\n", float(i) / elapsed, (8.0 * recv / elapsed)/1'000'000);
+        }
+
+        if (i % 50 == 0 && i != 0 && recv == prev_recv) {
+            break;
+        }
+
+        if (i % 50 == 0) prev_recv = recv;
     }
 
-    delete payload;
-
+    int elapsed = elapsed_us(gst);
+    printf("TIME: %.0f", float(elapsed) / 1'000'000);
+    
+    free(raw_bytes);
     return 0;
 }
 
